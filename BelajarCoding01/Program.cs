bff8d772a416cdf9185945e91c29b34213f1ed4c
Leo W <leowidodo037@gmail.com>
2021-07-20 19:51:36 +0700

    Add project files.

---
+++ BelajarCoding01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarCoding01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var acc = new AccountPayable();
+            acc.AddPays(new employee());
+            acc.AddPays(new Contractor());
+            acc.Pays();
+            Console.ReadKey();
+        }
+
+        class AccountPayable
+        {
+            private IList<Worker> workers = new List<Worker>();
+
+            internal void AddPays(Worker worker)
+            {
+                workers.Add(worker);
+            }
+
+            internal void Pays()
+            {
+                foreach (var work in workers)
+                {
+                    work.PaySalary();
+                }
+            }
+        }
+
+        abstract class Worker
+        {
+            protected abstract string AcceptPayment();
+            internal void PaySalary()
+            {
+                Console.WriteLine($"{AcceptPayment()}");
+            }
+        }
+
+        class employee : Worker
+        {
+            protected override string AcceptPayment()
+            {
+                return "Employee";
+            }
+        }
+        class Contractor : Worker
+        {
+            protected override string AcceptPayment()
+            {
+                return "Contractor";
+            }
+        }
+
+    }
+}
